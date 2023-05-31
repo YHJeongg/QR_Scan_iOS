16ba915164c7a01498e2507699c41299d031f3ff
YunHyeonJeong <wjd6408@gmail.com>
2023-05-31 14:36:54 +0900

    First Project

---
+++ QRScan/QRScan/QRScanApp.swift
@@ -0,0 +1,17 @@
+//
+//  QRScanApp.swift
+//  QRScan
+//
+//  Created by Jeong Yun Hyeon on 2023/05/31.
+//
+
+import SwiftUI
+
+@main
+struct QRScanApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
